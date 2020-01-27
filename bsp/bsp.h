9fab06d2e1564cf388b861ce378cb4c485477c1d
marshalltaylor <marshall.taylor@sparkfun.com>
2020-01-26 19:54:23 -0700

    Connected gpio between bsp and hal

--- bsp/bsp.h
+++ bsp/bsp.h
@@ -3,6 +3,7 @@
 
 // This file is an interface and should not include other files
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -12,7 +13,63 @@ extern "C" {
 void bspInit(void);
 
 /* IO */
+#define D0 0
+#define D1 1
+#define D2 2
+#define D3 3
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+#define D8 8
+#define D9 9
+#define D10 10
+#define D11 11
+#define D12 12
+#define D13 13
+#define D14 14
+#define D15 15
+#define D16 16
+#define D17 17
+#define D18 18
+#define D19 19
+#define D20 20
+#define D21 21
+#define D22 22
+#define D23 23
+#define D24 24
+#define D25 25
+#define D26 26
+#define D27 27
+#define D28 28
+#define D29 29
+#define D30 30
+#define D31 31
+#define D32 32
+#define D33 33
+#define D34 34
+#define D35 35
+#define D36 36
+
+#define A0 D16
+#define A1 D17
+#define A2 D18
+#define A3 D19
+#define A4 D20
+#define A5 D21
+
+//TODO: Used for init function but not tested
+#define BSP_IO_OUTPUT 1
+#define BSP_IO_INPUT 2
+#define BSP_IO_INPUT_PULLUP 2
+#define BSP_IO_INPUT_PULLDOWN 3
+
 void bspIOToggleLED(void);
+
+void bspIOPinInit(uint8_t pin, uint8_t pullMode);
+void bspIOPinWrite(uint8_t pin, bool value);
+bool bspIOPinRead(uint8_t pin);
+
 void bspADCConvert(void);
 //extern uint16_t adcValues[10];
 
--- bsp/bsp_dot2/bspIO.c
+++ bsp/bsp_dot2/bspIO.c
@@ -8,6 +8,7 @@
 
 /* Includes -- BSP -----------------------------------------------------------*/
 #include "bsp.h"
+#include "bspPins.h"
 
 /* References ----------------------------------------------------------------*/
 static bool ledState = false;
@@ -17,4 +18,32 @@ void bspIOToggleLED(void)
 {
 	ledState =! ledState;
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, ledState);
+}
+
+// defaulted in bsp, this can be used to override pulls
+void bspIOPinInit(uint8_t pin, uint8_t pullMode)
+{
+//	GPIO_InitTypeDef GPIO_InitStruct;
+//	
+//	GPIO_InitStruct.Pin = DxToPin[pin];
+//	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+//	GPIO_InitStruct.Pull = GPIO_NOPULL;
+//	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+//	HAL_GPIO_Init((GPIO_TypeDef *) (AHB1PERIPH_BASE + DxToPort[pin]), &GPIO_InitStruct);
+}
+
+void bspIOPinWrite(uint8_t pin, bool value)
+{
+	GPIO_PinState bspValue = GPIO_PIN_RESET;
+	if( value ) bspValue = GPIO_PIN_SET;
+	HAL_GPIO_WritePin((GPIO_TypeDef *) (AHB1PERIPH_BASE + DxToPort[pin]), DxToPin[pin], bspValue);
+}
+
+bool bspIOPinRead(uint8_t pin)
+{
+	if( HAL_GPIO_ReadPin((GPIO_TypeDef *) (AHB1PERIPH_BASE + DxToPort[pin]), DxToPin[pin]) == GPIO_PIN_SET )
+	{
+		return true;
+	}
+	return false;
 }
