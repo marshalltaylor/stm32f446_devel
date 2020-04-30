7becff45c87127d5683dab0b7e1e038fb1a102f4
marshalltaylor <marshall.taylor@sparkfun.com>
2020-04-29 21:56:23 -0600

    I think I got some console output stablizied

--- bsp/UserProject/Core/Src/main_cubemx.c
+++ bsp/UserProject/Core/Src/main_cubemx.c
@@ -131,7 +131,7 @@ int main_cubemx(void)
   //MX_USART2_UART_Init();
   MX_USART6_UART_Init();
   MX_TIM2_Init();
-  MX_TIM3_Init();
+  //MX_TIM3_Init();
   MX_TIM4_Init();
   MX_DAC_Init();
   MX_TIM6_Init();
